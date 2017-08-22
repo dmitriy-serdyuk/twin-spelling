5e5efb1988490903ec32bb10b782a3d2fabb4122
dmitriy-serdyuk <serdyuk.dmitriy@gmail.com>
2017-08-22 18:00:24 -0400

    Add spellchecking dataset

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 import string
+import numpy
 from itertools import chain
 from torch.utils.data import Dataset
 
@@ -19,4 +20,24 @@ class Penntree(Dataset):
         return len(self.data) // self.length
 
     def __getitem__(self, i):
-        return self.data[i * self.length:(i + 1) * self.length]
+        return numpy.array(self.data[i * self.length:(i + 1) * self.length])
+
+
+class SpellCheckData(Penntree):
+    def __init__(self, length, subset, prob, seed=None):
+        if seed is None:
+            seed = 426
+
+        self.rng = numpy.random.RandomState(seed)
+        super(SpellCheckData, self).__init__(length, subset)
+
+        self.prob = prob
+
+    def __getitem__(self, i):
+        target = super(SpellCheckData, self).__getitem__(i)
+        input = target.copy()
+        indeces = self.rng.binomial(1, self.prob, size=input.shape)
+        substitute = self.rng.choice(list(rev_vocabulary.keys()), size=input.shape)
+        input =  substitute * indeces + input * (1 - indeces)
+
+        return input, target
