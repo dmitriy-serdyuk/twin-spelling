3ef7381807b1538a80c05251af6dd8fd5a1802b5
dmitriy-serdyuk <serdyuk.dmitriy@gmail.com>
2017-08-22 17:36:39 -0400

    Create pytorch dataset

--- dataset.py
+++ dataset.py
@@ -1,16 +1,22 @@
 import string
 from itertools import chain
+from torch.utils.data import Dataset
 
 punctuation = " -.''1234567890&$#\\/*"
 characters = string.ascii_lowercase + punctuation + "N" + "<" + ">" + "\n"
 vocabulary = {c: ind for ind, c in enumerate(characters)}
 rev_vocabulary = {ind: c for ind, c in enumerate(characters)}
 
-with open("data/penn/train.txt", "r") as f:
-    train_data = list(chain(*[[vocabulary[c] for c in seq] for seq in f]))
 
-with open("data/penn/valid.txt", "r") as f:
-    valid_data = list(chain(*[[vocabulary[c] for c in seq] for seq in f]))
+class Penntree(Dataset):
+    def __init__(self, length, subset):
+        self.subset = subset
+        self.length = length
+        with open("data/penn/{}.txt".format(subset), "r") as f:
+            self.data = list(chain(*[[vocabulary[c] for c in seq] for seq in f]))
 
-with open("data/penn/test.txt", "r") as f:
-    test_data = list(chain(*[[vocabulary[c] for c in seq] for seq in f]))
+    def __len__(self):
+        return len(self.data) // self.length
+
+    def __getitem__(self, i):
+        return self.data[i * self.length:(i + 1) * self.length]
